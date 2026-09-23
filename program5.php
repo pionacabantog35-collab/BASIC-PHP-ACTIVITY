77d1212c69433f5182a6730cf55674fafcbd4c21
Piona Cabantog <pionacabantog35@gmail.com>
2026-09-23 22:56:01 +0800

    Create program5.php

---
+++ program5.php
@@ -0,0 +1,16 @@
+<?php
+
+$name = "Piona";
+$age = 21;
+$course = "BS Computer Science";
+$school = "Arellano University";
+
+echo "Name: " . $name . "<br>";
+echo "Age: " . $age . "<br>";
+echo "Course: " . $course . "<br>";
+echo "School: " . $school . "<br>";
+
+echo "<br>";
+echo "Hello! I am " . $name . " and I am learning PHP.";
+
+?>
